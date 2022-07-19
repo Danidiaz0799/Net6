92b7d11e1da5409b07a976b531cd15120d52d69c
Daniel <danidiaz0799@gmail.com>
2022-07-19 11:25:35 -0500

    Primer commit importante

---
+++ CrudEFCoreNet6/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using CrudEFCoreNet6.Datos;
+using CrudEFCoreNet6.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace CrudEFCoreNet6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDBContext _contexto;
+
+        public HomeController(ApplicationDBContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _contexto.Usuario.ToListAsync());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CrudEFCoreNet6/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudEFCoreNet6.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int id { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El telefono es obligatorio")]
+        public string Telefono { get; set; }
+        [Required(ErrorMessage = "El celular es obligatorio")]
+        public string Celular { get; set; }
+        [Required(ErrorMessage = "El email es obligatorio")]
+        public string Email { get; set; }
+
+    }
+}
