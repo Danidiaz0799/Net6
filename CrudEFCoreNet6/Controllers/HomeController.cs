4a0ceb13e8fe285bf43a56d4aeefb840136deb8a
Steven Diaz <90877963+Danidiaz0799@users.noreply.github.com>
2022-07-19 12:51:05 -0500

    Se agregan las funciones del CRUD desde PC Job

--- CrudEFCoreNet6/Controllers/HomeController.cs
+++ CrudEFCoreNet6/Controllers/HomeController.cs
@@ -21,6 +21,25 @@ namespace CrudEFCoreNet6.Controllers
             return View(await _contexto.Usuario.ToListAsync());
         }
 
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Crear(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _contexto.Usuario.Add(usuario);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
